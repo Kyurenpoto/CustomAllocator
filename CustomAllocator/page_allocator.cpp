7e3a08d86ec915e2922f94ec45ca7516edd97974
Kyurnepoto <heal9179@gmail.com>
2019-04-28 23:57:53 +0900

    feature #1 - Add API for allocator

--- CustomAllocator/page_allocator.cpp
+++ CustomAllocator/page_allocator.cpp
@@ -2,7 +2,113 @@
 
 #include "page_allocator.h"
 
-struct page_allocator
-{
+#include <cstddef>
 
-};
+namespace
+{
+    constexpr std::size_t SIZE_PAGE = 4 * 1024;
+    constexpr std::size_t SIZE_AREA_MIN = 64 * 1024;
+    constexpr std::size_t SIZE_AREA_MAX = 4ull * 1024 * 1024 * 1024;
+
+    std::size_t unitize(const std::size_t unit, const std::size_t target)
+    {
+        assert(unit > 0 &&
+               target > 0);
+
+        return target - target % unit;
+    }
+
+    struct page_allocator
+    {
+        page_allocator() = default;
+
+        ~page_allocator()
+        {
+            dispose();
+        }
+
+        AreaAllocator::AreaResult initiate(
+            std::size_t sizeAreaMin,
+            std::size_t sizeMemoryMax)
+        {
+            try
+            {
+                assert(_sizeAreaMin == 0 && _sizeMemoryMax == 0);
+            }
+            catch (...)
+            {
+                return AreaAllocator::AreaResult::ALREADY_INITIATED;
+            }
+
+            try
+            {
+                _sizeAreaMin = unitize(sizeAreaMin, SIZE_PAGE);
+                _sizeMemoryMax = unitize(sizeMemoryMax, SIZE_PAGE);
+            }
+            catch(...)
+            {
+                return AreaAllocator::AreaResult::UNDEFINED;
+            }
+
+            try
+            {
+                _sizeMemoryMax = unitize(_sizeMemoryMax, _sizeAreaMin);
+                
+                assert(sizeMemoryMax > sizeAreaMin);
+
+                return AreaAllocator::AreaResult::SUCCESSED;
+            }
+            catch(...)
+            {
+                return AreaAllocator::AreaResult::UNDEFINED;
+            }
+        }
+        
+        AreaAllocator::AreaResult dispose()
+        {
+            return AreaAllocator::AreaResult::SUCCESSED;
+        }
+        
+        AreaAllocator::AreaResult allocate(
+            AreaAllocator::memory_area & target)
+        {
+            return AreaAllocator::AreaResult::SUCCESSED;
+        }
+        
+        AreaAllocator::AreaResult deallocate(
+            AreaAllocator::memory_area & target)
+        {
+            return AreaAllocator::AreaResult::SUCCESSED;
+        }
+
+    private:
+        std::size_t _sizeAreaMin = 0;
+        std::size_t _sizeMemoryMax = 0;
+    };
+
+    static page_allocator allocator;
+}
+
+namespace AreaAllocator
+{
+    AreaResult Initiate(const std::size_t sizeAreaMin,
+                        const std::size_t sizeMemoryMax)
+    {
+        return allocator.initiate(sizeAreaMin, sizeMemoryMax);
+    }
+
+    AreaResult Dispose()
+    {
+        return allocator.dispose();
+    }
+
+    AreaResult Allocate(memory_area & target)
+    {
+        return allocator.allocate(target);
+    }
+    
+    AreaResult Deallocate(memory_area & target)
+    {
+        return allocator.deallocate(target);
+    }
+}
--- CustomAllocator/page_allocator.h
+++ CustomAllocator/page_allocator.h
@@ -1,7 +1,26 @@
 #pragma once
 
-struct memory_area
+namespace AreaAllocator
 {
-    void * location;
-    std::size_t size;
-};
+    struct memory_area
+    {
+        void* location;
+        std::size_t size;
+    };
+
+    enum class AreaResult
+    {
+        SUCCESSED,
+        ALREADY_INITIATED,
+        ALREADY_DISPOSED,
+        RUN_OUT,
+        EMPTY,
+        UNDEFINED
+    };
+
+    AreaResult Initiate(const std::size_t sizeAreaMin,
+                        const std::size_t sizeMemoryMax);
+    AreaResult Dispose();
+    AreaResult Allocate(memory_area & target);
+    AreaResult Deallocate(memory_area & target);
+}
