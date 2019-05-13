f79d264a8b46dcf675a64a5e0a1a8d7f97977d49
Kyurnepoto <heal9179@gmail.com>
2019-05-14 00:31:03 +0900

    feature #3 - Implement new API & remove deprecated APIs

--- CustomAllocator/includes/area_allocator/virtual_memory_wrapper.h
+++ CustomAllocator/includes/area_allocator/virtual_memory_wrapper.h
@@ -4,17 +4,8 @@
 
 namespace virtual_memory
 {
-    [[deprecated]]
-	void *    reserve(std::size_t nPage);
-    [[deprecated]]
-	void *    commit(void * ptr, std::size_t nPage);
-    [[deprecated]]
-	void      release(void * ptr);
-    [[deprecated]]
-	void      decommit(void* ptr, std::size_t nPage);
-
     void *    alloc(std::size_t nPage);
-    void      dealloc(void * addr, std::size_t nPage);
+    void      dealloc(void * addr);
 
     struct AddrInfo
     {
--- CustomAllocator/src/area_allocator/virtual_memory_wrapper.cpp
+++ CustomAllocator/src/area_allocator/virtual_memory_wrapper.cpp
@@ -7,34 +7,6 @@ namespace
 	constexpr std::size_t PAGE_SIZE = 4 * 1024;
 }
 
-void * virtual_memory::reserve(std::size_t nPage)
-{
-	void* reserved = VirtualAlloc(nullptr, nPage * PAGE_SIZE, MEM_RESERVE, PAGE_READWRITE);
-
-    assert(reserved != nullptr);
-
-	return reserved;
-}
-
-void * virtual_memory::commit(void * ptr, std::size_t nPage)
-{
-	void* commited = VirtualAlloc(ptr, nPage * PAGE_SIZE, MEM_COMMIT, PAGE_READWRITE);
-
-    assert(commited != nullptr);
-
-	return commited;
-}
-
-void virtual_memory::release(void * ptr)
-{
-    assert(VirtualFree(ptr, 0, MEM_RELEASE));
-}
-
-void virtual_memory::decommit(void * ptr, std::size_t nPage)
-{
-    assert(VirtualFree(ptr, nPage * PAGE_SIZE, MEM_DECOMMIT));
-}
-
 void* virtual_memory::alloc(std::size_t nPage)
 {
     void * addr = VirtualAlloc(nullptr, nPage * PAGE_SIZE,
@@ -45,7 +17,33 @@ void* virtual_memory::alloc(std::size_t nPage)
     return addr;
 }
 
-void virtual_memory::dealloc(void * addr, std::size_t nPage)
+void virtual_memory::dealloc(void * addr)
 {
-    assert(VirtualFree(addr, nPage * PAGE_SIZE, MEM_RELEASE));
+    assert(VirtualFree(addr, 0, MEM_RELEASE));
+}
+
+namespace
+{
+    virtual_memory::AddrInfo::AddrState stateWin2Enum(DWORD state)
+    {
+        switch (state)
+        {
+        case MEM_FREE:
+            return virtual_memory::AddrInfo::AddrState::FREE;
+        case MEM_RESERVE:
+            return virtual_memory::AddrInfo::AddrState::RESERVE;
+        case MEM_COMMIT:
+            return virtual_memory::AddrInfo::AddrState::COMMIT;
+        default:
+            return virtual_memory::AddrInfo::AddrState::FREE;
+        }
+    }
+}
+
+virtual_memory::AddrInfo virtual_memory::getAddrInfo(void * addr)
+{
+    MEMORY_BASIC_INFORMATION mbi;
+    VirtualQuery(addr, &mbi, sizeof(mbi));
+
+    return AddrInfo{ mbi.RegionSize / PAGE_SIZE, stateWin2Enum(mbi.State) };
 }
