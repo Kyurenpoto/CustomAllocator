1c0a6dcad12e7948c1776ae8e8b1660ea26f706c
Kyurenpoto <heal9179@gmail.com>
2019-05-17 17:33:00 +0900

    feature #1 - Code spec of class 'area_info'

--- CustomAllocator/includes/area_allocator/area_info.h
+++ CustomAllocator/includes/area_allocator/area_info.h
@@ -1,31 +1,70 @@
 #pragma once
 
 #include <cstddef>
+#include <functional>
+#include <new>
 
 namespace AreaAllocator
 {
     struct area_info
     {
         template<class T, class... Args>
-        void construct(const Args&& ... args);
+        void construct(const Args&& ... args)
+        {
+            constructImpl(sizeof(T),
+                          [](void * addr) { new(addr) T{ args... }; });
+        }
 
         template<class T>
-        void destruct();
+        void destruct()
+        {
+            destructImpl(sizeof(T), [](void* addr) { (T*)addr->~T(); });
+        }
 
         virtual std::size_t getSizeUsed() const noexcept;
         std::size_t getSizeTotal() const noexcept;
-
         std::size_t getAreaId() const noexcept;
 
     protected:
         area_info(std::size_t areaId);
 
-        template<class T, class... Args>
-        void constructAt(const std::size_t offset, const Args&& ... args);
-
-        template<class T>
-        void destructAt(const std::size_t offset);
+        virtual void constructImpl(const std::size_t sizeType,
+                                   void (* constructor)(void *)) const;
+        virtual void destructImpl(const std::size_t sizeType,
+                                  void (* destructor)(void *)) const;
 
         std::size_t _areaId;
+    };
+
+    struct area_stack_info :
+        public area_info
+    {
+        area_stack_info(std::size_t areaId);
+
+        std::size_t getSizeUsed() const noexcept override final;
+
+    protected:
+        void constructImpl(const std::size_t sizeType,
+                           void (* constructor)(void *)) const override final;
+        void destructImpl(const std::size_t sizeType,
+                          void (* destructor)(void *)) const override final;
+
+        std::size_t _top = 0;
+    };
+
+    struct area_queue_info :
+        public area_info
+    {
+        area_queue_info(std::size_t areaId);
+
+        std::size_t getSizeUsed() const noexcept override final;
+    
+    protected:
+        void constructImpl(const std::size_t sizeType,
+                           void (* constructor)(void *)) const override final;
+        void destructImpl(const std::size_t sizeType,
+                          void (* destructor)(void *)) const override final;
+
+        std::size_t _front = 0, _rear = 0;
     };
 }
--- CustomAllocator/src/area_allocator/area_info.cpp
+++ CustomAllocator/src/area_allocator/area_info.cpp
@@ -1,5 +1,14 @@
 #include "stdafx.h"
 
 #include "includes/area_allocator/area_info.h"
-
 #include "includes/area_allocator/area_access.h"
+
+std::size_t AreaAllocator::area_info::getSizeTotal() const noexcept
+{
+    return AreaAccess::getSizeTotal(_areaId);
+}
+
+std::size_t AreaAllocator::area_info::getAreaId() const noexcept
+{
+    return _areaId;
+}
